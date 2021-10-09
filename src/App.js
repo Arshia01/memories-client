b4259e328d19154f4c467130db6a159debcca48e
Arshia01 <arshiavashisht06@gmail.com>
2021-10-09 15:16:59 +0530

    make navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {Container} from "@material-ui/core"
-import Posts from "./components/Posts/Posts";
 import Navbar from "./components/Navbar/Navbar";
-import Form from "./components/Form/Form";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 import { BrowserRouter, Switch, Route } from "react-router-dom"; //for creating multi page app
